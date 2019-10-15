a4e38293635a934dc29a61d72d05c7c9a54eeac4
Luis Joel Caballero <luijo@outlook.com>
2019-10-14 21:29:03 -0500

    Wins now get +1 when Score to get is equal to Score so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 // Set the variable = to function to call it
 var scoreToGet= scoreGoal();
 var scoreSoFar= 0;
+var greenDiamond, redDiamond, blueDiamond, brownDiamond
+var loses= 0;
+var wins= 0;
 function scoreGoal () {
 // Setting the random number between 1 and 240, then adding 10 so it can get to 250
     return Math.floor(Math.random()*102)+19
@@ -10,11 +13,13 @@ function scoreGoal () {
 console.log(scoreToGet);
 // Displaying the random number on "Score to get"
 $("#scoreToGet").text(scoreToGet);
-var greenDiamond = green();
-var redDiamond= red();
-var blueDiamond= blue();
-var brownDiamond= brown();
-
+function diamondsValue() {
+    greenDiamond = green();
+    redDiamond= red();
+    blueDiamond= blue();
+    brownDiamond= brown();   
+}
+diamondsValue();
 function green () {
     return Math.floor(Math.random()*12)+1
 }
@@ -32,6 +37,13 @@ $("#green").attr('data-value', greenDiamond);
 $("#red").attr('data-value', redDiamond);
 $("#blue").attr('data-value', blueDiamond);
 $("#brown").attr('data-value', brownDiamond);
+
+function resetGame() {
+    scoreToGet= scoreGoal();
+    scoreSoFar=0;
+    diamondsValue();
+}
+
 // On click event to call each diamond function (function green, function blue...)
 $(".diamonds").on("click", function(event){
 // Console log of $(this) to make sure $(".diamonds") is working to assign a value to each diamond and being the same each click
@@ -39,6 +51,15 @@ $(".diamonds").on("click", function(event){
     scoreSoFar += parseInt($(this).attr('data-value'));
     console.log(scoreSoFar);
 $("#scoreSoFar").text(scoreSoFar);
+if(scoreSoFar === scoreToGet){
+    console.log("You won!");
+    wins++;
+    $("#wins").text(wins);
+}else if (scoreSoFar > scoreToGet){
+    loses++;
+    resetGame();
+    $("loses").text(loses);
+}
   });
 
 });
